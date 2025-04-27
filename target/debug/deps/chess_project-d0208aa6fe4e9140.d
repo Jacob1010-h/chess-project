0d8efd2b2382b495675edf6d72b1e51867759058
Jacob1010-h <jacobhotz01@gmail.com>
2025-04-27 15:13:43 -0600

    add rust.yml

--- target/debug/deps/chess_project-d0208aa6fe4e9140.d
+++ target/debug/deps/chess_project-d0208aa6fe4e9140.d
@@ -1,8 +1,10 @@
-/home/jacobh/Documents/chess-project/target/debug/deps/chess_project-d0208aa6fe4e9140: src/main.rs src/app.rs src/event.rs src/ui.rs
+/home/jacobh/Documents/chess-project/target/debug/deps/chess_project-d0208aa6fe4e9140: src/main.rs src/app.rs src/event.rs src/game.rs src/pieces.rs src/ui.rs
 
-/home/jacobh/Documents/chess-project/target/debug/deps/chess_project-d0208aa6fe4e9140.d: src/main.rs src/app.rs src/event.rs src/ui.rs
+/home/jacobh/Documents/chess-project/target/debug/deps/chess_project-d0208aa6fe4e9140.d: src/main.rs src/app.rs src/event.rs src/game.rs src/pieces.rs src/ui.rs
 
 src/main.rs:
 src/app.rs:
 src/event.rs:
+src/game.rs:
+src/pieces.rs:
 src/ui.rs:
